e74c5a29dcc740fda3f91938e737c712f9e3b944
Michael Baudino <michael.baudino@alpine-lab.com>
2015-05-13 14:51:07 +0200

    Make completion dynamic (options depend on the command) Also, update available commands & options thanks to a script (provided)

--- heroku_bash_completion.sh
+++ heroku_bash_completion.sh
@@ -1,49 +1,27 @@
 __heroku_complete_base()
 {
   local current_word previous_word
-  local general_commands app_commands all_commands
-  local app_options db_options
 
-  COMPREPLY=()
-
-  current_word="${COMP_WORDS[COMP_CWORD]}"
-  previous_word="${COMP_WORDS[COMP_CWORD-1]}"
-
-  general_commands="help version list create keys keys:add keys:remove keys:clear"
-  app_commands="info open rename dynos workers sharing:add sharing:remove sharing:transfer domains:add domains:remove domains:clear ssl:add ssl:remove rake console restart logs logs:cron maintenance:on maintenance:off config config:add config:remove config:clear stack stack:migrate bundles bundles:capture bundles:download bundles:download bundles:animate bundles:destroy addons addons:info addons:add addons:remove addons:clear destroy plugins plugins:install plugins:uninstall"
-  db_commands="db:pull db:push db:reset"
-  all_commands="${general_commands} ${app_commands} ${db_commands}"
-
-  app_options="--app --remote --stack --timeout --addons"
-  db_options="--force --chunksize --filter --tables --disable-compression --resume-filename --indexes-last --debug"
+  _get_comp_words_by_ref -n ':' -c current_word -p previous_word
 
   case "${previous_word}" in
     --app|-a)
-      COMPREPLY=($(compgen -W "$(__heroku_apps)" -- ${current_word}))
+      COMPREPLY=($(compgen -W '$(__heroku_apps)' -- ${current_word}))
       return 0
       ;;
     rake)
-      COMPREPLY=($(compgen -W "$(__rake_tasks)" -- ${current_word}))
+      COMPREPLY=($(compgen -W '$(__rake_tasks)' -- ${current_word}))
       return 0
       ;;
   esac
 
-  if [[ $app_commands == *$previous_word* ]]; then
-    COMPREPLY=($(compgen -W "${app_options}" -- ${current_word}))
+  if [[ ${current_word} == -* ]]; then
+    COMPREPLY=($(compgen -W '${__heroku_commands_and_options["${previous_word}"]}' -- ${current_word}))
     return 0
   fi
 
-  if [[ $db_commands == *$previous_word* ]]; then
-    COMPREPLY=($(compgen -W "${db_options}" -- ${current_word}))
-    return 0
-  fi
-
-  if [[ ${current_word} == -* ]] ; then
-    COMPREPLY=($(compgen -W "${app_options}" -- ${current_word}))
-    return 0
-    fi
-
-  COMPREPLY=($(compgen -W "${all_commands}" -- ${current_word}))
+  COMPREPLY=($(compgen -W '${!__heroku_commands_and_options[@]}' -- ${current_word}))
+  __ltrim_colon_completions "$current_word"
 }
 
 __heroku_apps()
@@ -57,3 +35,148 @@ __rake_tasks()
 }
 
 complete -F __heroku_complete_base heroku
+
+declare -A __heroku_commands_and_options=(
+  ["addons"]="--all --app --resource "
+  ["addons:attach"]="--as --confirm "
+  ["addons:create"]="--name --as --confirm "
+  ["addons:destroy"]="--force "
+  ["addons:detach"]=""
+  ["addons:docs"]=""
+  ["addons:downgrade"]=""
+  ["addons:open"]=""
+  ["addons:plans"]=""
+  ["addons:services"]=""
+  ["addons:upgrade"]=""
+  ["apps"]=""
+  ["apps:create"]="--addons --buildpack --no-remote --remote --stack --region --locked --ssh-git "
+  ["apps:destroy"]=""
+  ["apps:info"]="--shell "
+  ["apps:join"]="--app "
+  ["apps:leave"]="--app "
+  ["apps:lock"]=""
+  ["apps:open"]=""
+  ["apps:rename"]="--ssh-git "
+  ["apps:unlock"]=""
+  ["auth"]=""
+  ["auth:login"]=""
+  ["auth:logout"]=""
+  ["auth:password"]=""
+  ["auth:token"]=""
+  ["auth:user"]=""
+  ["auth:whoami"]=""
+  ["config"]=""
+  ["config:get"]="--shell "
+  ["config:set"]=""
+  ["config:unset"]=""
+  ["domains"]=""
+  ["domains:add"]=""
+  ["domains:clear"]=""
+  ["domains:remove"]=""
+  ["logs"]=""
+  ["ps"]=""
+  ["ps:resize"]=""
+  ["ps:restart"]=""
+  ["ps:scale"]=""
+  ["ps:stop"]=""
+  ["ps:type"]=""
+  ["releases"]=""
+  ["releases:info"]="--shell "
+  ["releases:rollback"]=""
+  ["run"]="--exit-code "
+  ["run:detached"]="--size --tail "
+  ["run:rake"]=""
+  ["sharing"]=""
+  ["sharing:add"]=""
+  ["sharing:remove"]=""
+  ["sharing:transfer"]="--locked "
+  ["accounts"]=""
+  ["accounts:add"]="--auto "
+  ["accounts:default"]=""
+  ["accounts:remove"]=""
+  ["accounts:set"]=""
+  ["buildpacks"]=""
+  ["buildpacks:add"]="--index "
+  ["buildpacks:clear"]=""
+  ["buildpacks:remove"]="--index "
+  ["buildpacks:set"]="--index "
+  ["certs"]=""
+  ["certs:add"]="--bypass "
+  ["certs:chain"]=""
+  ["certs:generate"]="--city --selfsigned --keysize --owner --country --area --city --subject --now "
+  ["certs:info"]="--endpoint "
+  ["certs:key"]=""
+  ["certs:remove"]="--endpoint "
+  ["certs:rollback"]="--endpoint "
+  ["certs:update"]="--bypass --endpoint "
+  ["drains"]=""
+  ["drains:add"]=""
+  ["drains:remove"]=""
+  ["features"]=""
+  ["features:disable"]=""
+  ["features:enable"]=""
+  ["features:info"]=""
+  ["fork"]="--region --skip-pg "
+  ["git"]=""
+  ["git:clone"]="--app --remote --ssh-git "
+  ["git:remote"]="--app --remote --ssh-git "
+  ["help"]="--addons "
+  ["keys"]=""
+  ["keys:add"]=""
+  ["keys:clear"]=""
+  ["keys:remove"]=""
+  ["labs"]=""
+  ["labs:disable"]=""
+  ["labs:enable"]=""
+  ["labs:info"]=""
+  ["local"]=""
+  ["maintenance"]=""
+  ["maintenance:off"]=""
+  ["maintenance:on"]=""
+  ["members"]=""
+  ["members:add"]="--role "
+  ["members:remove"]=""
+  ["members:set"]="--role "
+  ["orgs"]=""
+  ["orgs:open"]=""
+  ["pg"]=""
+  ["pg:backups"]="--quiet --at "
+  ["pg:copy"]=""
+  ["pg:credentials"]="--reset "
+  ["pg:diagnose"]=""
+  ["pg:info"]="--extended "
+  ["pg:kill"]=""
+  ["pg:killall"]=""
+  ["pg:maintenance"]="--force "
+  ["pg:promote"]=""
+  ["pg:ps"]=""
+  ["pg:psql"]="--command "
+  ["pg:pull"]=""
+  ["pg:push"]=""
+  ["pg:reset"]=""
+  ["pg:unfollow"]=""
+  ["pg:upgrade"]=""
+  ["pg:wait"]="--wait-interval "
+  ["pgbackups"]=""
+  ["pgbackups:capture"]="--expire "
+  ["pgbackups:destroy"]=""
+  ["pgbackups:restore"]=""
+  ["pgbackups:transfer"]=""
+  ["pgbackups:url"]=""
+  ["plugins"]=""
+  ["plugins:install"]=""
+  ["plugins:link"]=""
+  ["plugins:uninstall"]=""
+  ["plugins:update"]=""
+  ["redis"]=""
+  ["regions"]=""
+  ["stack"]=""
+  ["stack:set"]=""
+  ["status"]=""
+  ["twofactor"]=""
+  ["twofactor:disable"]=""
+  ["twofactor:generate-recovery-codes"]=""
+  ["update"]=""
+  ["update:beta"]=""
+  ["version"]=""
+)
